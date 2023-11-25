9aa20f2bb9458aaa65a47b28f3f6db07ba331ea6
XPS\AITS <nguyenhothanh87>
2023-11-25 14:27:55 +0700

    commit

--- src/app/pipe-reverse.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PipeReversePipe } from './pipe-reverse.pipe';
-
-describe('PipeReversePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PipeReversePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
